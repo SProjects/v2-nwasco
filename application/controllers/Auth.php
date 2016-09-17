5c00405a4660bc31937f93a1edf8445229380dc1
Sebuuma Daniel <sedzsoft@gmail.com>
2016-09-17 14:46:03 +0300

    Fixes schemes loading error on edit_user page.

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -493,6 +493,7 @@ EOF;
             $this->data['user'] = $this->ion_auth->user()->row();
             $this->data['utilities'] = $this->core->getAllUtilities();
             $this->data['indicators'] = $this->core->getIndicators();
+            $this->data['schemes']  = $this->core->getSchemes();
             // render
             $this->_render_page('header', $this->data);
             $this->_render_page('user_nav', $this->data);
@@ -790,6 +791,7 @@ EOF;
         $this->data['user'] = $this->ion_auth->user()->row();
         $this->data['utilities'] = $this->core->getAllUtilities();
         $this->data['indicators'] = $this->core->getIndicators();
+        $this->data['schemes']  = $this->core->getSchemes();
 
         $this->_render_page('header', $this->data);
         $this->_render_page('user_nav', $this->data);
