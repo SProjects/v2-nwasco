7de03563e5d08ff3a03174f1782d825203b9642e
Sebuuma Daniel <sedzsoft@gmail.com>
2016-09-22 11:50:40 +0300

    Indents to make code cleaner

--- application/views/auth/create_user.php
+++ application/views/auth/create_user.php
@@ -6,43 +6,43 @@
     <?php endif; ?>
 
     <?php echo form_open("auth/create_user", "id='branddet'"); ?>
-    <p>
-        <?php echo form_input($first_name); ?>
-    </p>
-    <p>
-        <?php echo form_input($last_name); ?>
-    </p>
-    <?php
-    if ($identity_column !== 'email') {
-        echo lang('create_user_identity_label', 'identity');
-        echo '<br />';
-        echo form_error('identity');
-        echo form_input($identity);
-    }
-    ?>
-    <div class="row">
-        <div class="col-md-7">
-            <div class="input-group m-b"><span class="input-group-addon">@</span>
-                <?php echo form_input($email); ?>
+        <p>
+            <?php echo form_input($first_name); ?>
+        </p>
+        <p>
+            <?php echo form_input($last_name); ?>
+        </p>
+        <?php
+        if ($identity_column !== 'email') {
+            echo lang('create_user_identity_label', 'identity');
+            echo '<br />';
+            echo form_error('identity');
+            echo form_input($identity);
+        }
+        ?>
+        <div class="row">
+            <div class="col-md-7">
+                <div class="input-group m-b"><span class="input-group-addon">@</span>
+                    <?php echo form_input($email); ?>
+                </div>
+            </div>
+
+            <div class="col-md-5 form-group">
+                <?php echo form_input($phone); ?>
             </div>
         </div>
+        <div class="clearfix"></div>
+        <div class="row">
+            <div class="col-md-6 form-group">
+                <?php echo form_input($password); ?>
+            </div>
 
-        <div class="col-md-5 form-group">
-            <?php echo form_input($phone); ?>
+            <div class="col-md-6 form-group">
+                <?php echo form_input($password_confirm); ?>
+            </div>
         </div>
-    </div>
-    <div class="clearfix"></div>
-    <div class="row">
-        <div class="col-md-6 form-group">
-            <?php echo form_input($password); ?>
-        </div>
+        <div class="clearfix"></div>
 
-        <div class="col-md-6 form-group">
-            <?php echo form_input($password_confirm); ?>
-        </div>
-    </div>
-    <div class="clearfix"></div>
-
-    <p><?php echo form_submit('submit', lang('create_user_submit_btn'), 'class="btn btn-primary"'); ?></p>
+        <p><?php echo form_submit('submit', lang('create_user_submit_btn'), 'class="btn btn-primary"'); ?></p>
+    <?php echo form_close(); ?>
 </div>
-<?php echo form_close(); ?>
